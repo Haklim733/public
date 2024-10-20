3a97e571573db47caf0b2a59c2e3ea61d911fa3f
leehaklim <lee.haklim@gmail.com>
2024-10-19 19:47:36 -0700

    remove rtServer.subscribe

--- infra/realtime.ts
+++ infra/realtime.ts
@@ -5,6 +5,6 @@ export const rtServer = new sst.aws.Realtime(`IotServer`, {
 	authorizer: { handler, link: [rtToken] }
 });
 // There is only 1 IoT endpoint per region per AWS account. Messages from all apps and stages are published to the same IoT endpoint. Make sure to prefix the topics by the app and stage name.
-rtServer.subscribe('src/subscriber.handler', {
-	filter: `${$app.name}/${$app.stage}/iot/`
-});
+// rtServer.subscribe('src/subscriber.handler', {
+// 	filter: `${$app.name}/${$app.stage}/iot/`
+// });
