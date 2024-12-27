3297f854bd560521b07264754a260789cbc27d47
leehaklim <lee.haklim@gmail.com>
2024-12-26 23:35:48 -0800

    switch to us-west-1

--- sst.config.ts
+++ sst.config.ts
@@ -1,26 +1,26 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-	app(input) {
-		return {
-			name: 'public',
-			removal: 'remove',
-			home: 'aws',
-			providers: {
-				aws: {
-					profile: input.stage === 'production' ? 'myprod' : 'default',
-					region: 'us-east-1'
-				}
-			}
-		};
-	},
-	async run() {
-		const rtServer = await import('./infra/realtime');
-		const frontend = await import('./infra/frontend');
-		return {
-			app: frontend.site.url,
-			rtEndPoint: rtServer.rtServer.endpoint,
-			rtAuthorizer: rtServer.rtServer.authorizer
-		};
-	}
+  app(input) {
+    return {
+      name: "public",
+      removal: "remove",
+      home: "aws",
+      providers: {
+        aws: {
+          profile: input.stage === "production" ? "myprod" : "default",
+          region: "us-west-1",
+        },
+      },
+    };
+  },
+  async run() {
+    const rtServer = await import("./infra/realtime");
+    const frontend = await import("./infra/frontend");
+    return {
+      app: frontend.site.url,
+      rtEndPoint: rtServer.rtServer.endpoint,
+      rtAuthorizer: rtServer.rtServer.authorizer,
+    };
+  },
 });
